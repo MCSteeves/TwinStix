e1b4caa6d327a3b24c9c73c0aca2d810ff56e662
MCSteeves <32246649+MCSteeves@users.noreply.github.com>
2017-09-25 20:27:17 -0500

    Deleted Logs in PrintyScript

--- Assets/Scripts/PrintyScript.cs
+++ Assets/Scripts/PrintyScript.cs
@@ -6,8 +6,8 @@ public class PrintyScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("You did it");
-		Debug.Log ("You really did it now.");
+		
+
 	}
 	
 	// Update is called once per frame
